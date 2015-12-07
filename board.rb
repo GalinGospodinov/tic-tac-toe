9e4464329895952dd2638de55a1fde429a54a553
pecabum <galin.gospodinov@158ltd.com>
2015-12-07 10:56:14 +0200

    game is done

---
+++ board.rb
@@ -0,0 +1,24 @@
+class Board
+  attr_reader :board, :empty_cell
+
+  def initialize 
+    @empty_cell = '*' #default value for empty cell
+    @board = Array.new(9,@empty_cell)
+  end
+  
+  #print game board 
+  def print_grid
+    puts "\n"
+    @board.each_slice(3) {|row| puts row.join('|') }
+    puts "\n"
+  end
+
+  def update(pos, sym)
+    if @board[pos] == @empty_cell
+      @board[pos] = sym
+      return true
+    else
+      return false
+    end
+  end
+end
